ff34efeae5ba66dea76e2a824a4512e7e2332423
kiserysoma <47324548+kiserysoma@users.noreply.github.com>
2020-06-03 21:03:17 +0200

    Scripts for training RF classifiers added

---
+++ train_classifiers/create_dataset.py
@@ -0,0 +1,91 @@
+import os
+import cv2
+import pickle
+import random
+from collections import Counter
+from scipy import ndarray
+import skimage as sk
+from skimage import transform
+from skimage import util
+
+
+IMAGE_PATH = '/media/Projects/shared/face_dataset/'
+DST_PATH = '/media/Projects/shared/face_dataset_v2/'
+
+def copy_images():
+    imgs = pickle.load( open ('img_names.p', 'rb') )
+    IDs = []
+    indexes = []
+    buffer = []
+
+    for i in range(len(imgs)):
+        index = imgs[i][0:5].find('_')
+        ID = imgs[i][0:index]
+        if ID not in IDs:
+            indexes.append(i)
+            IDs.append(ID)
+        buffer.append(ID)
+
+    for i in range(len(IDs)):
+        for j in range(10):
+            if i != len(IDs)-1:
+                r1 = indexes[i]
+                r2 = indexes[i+1]
+            else:
+                r1 = indexes[i]
+                r2 = (len(imgs))
+            randn = random.randint(r1, r2)
+
+            chosen_img = imgs[randn]
+            img = cv2.imread(IMAGE_PATH + chosen_img)
+            cv2.imwrite(DST_PATH + chosen_img, img)
+
+def random_rotation(image_array: ndarray):
+    # pick a random degree of rotation between 25% on the left and 25% on the right
+    random_degree = random.uniform(-10, 10)
+    return sk.transform.rotate(image_array, random_degree)
+
+def random_noise(image_array: ndarray):
+    # add random noise to the image
+    return sk.util.random_noise(image_array)
+
+buffer = []
+IDs = []
+indexes = []
+original_images = sorted(os.listdir(DST_PATH))
+print(original_images)
+
+for i in range(len(original_images)):
+    index = original_images[i][0:5].find('_')
+    ID = original_images[i][0:index]
+    if ID not in IDs:
+        indexes.append(i)
+        IDs.append(ID)
+    buffer.append(ID)
+most_common = [item for item in Counter(buffer).most_common()]
+print(most_common)
+
+available_transformations = {
+    'rotate': random_rotation,
+    'noise': random_noise,
+}
+
+for i in range(len(IDs):
+	if i != len(IDs)-1:
+		current_images = original_images[indexes[i]:indexes[i+1]]
+    else:
+		current_images = original_images[indexes[len(indexes)-1]:]
+    to200 = 200-len(current_images)
+    print(to200)
+    for j in range(to200):
+        img_name = random.choice(current_images)
+        print(img_name)
+        img = cv2.imread(DST_PATH + img_name)
+        num_transformations_to_apply = random.randint(1, len(available_transformations))
+        num_transformations = 0
+        while num_transformations <= num_transformations_to_apply:
+            key = random.choice(list(available_transformations))
+            transformed_image = available_transformations[key](img)
+            num_transformations += 1
+        dst = DST_PATH + img_name[:len(img_name)-4] + "_" + str(j) + '.jpg'
+        cv2.imwrite(dst, transformed_image)
---
+++ train_classifiers/train_classifiers.py
@@ -0,0 +1,84 @@
+import os
+import pandas as pd
+import face_recognition
+import cv2
+import pickle
+from collections import Counter
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+from sklearn import metrics
+
+GET_EMBEDDINGS = True
+IMAGE_PATH = '/media/Projects/shared/face_dataset_v2/'
+
+images = sorted(os.listdir(IMAGE_PATH))
+IDs = []
+indexes = []
+buffer = []
+
+if GET_EMBEDDINGS: 
+    embeddings = []
+    imgs = []
+    i_ = 0
+    for i in images:
+        print(i_)
+        i_ += 1
+        img = cv2.imread(IMAGE_PATH + i)
+        embedding = face_recognition.api.face_encodings(img)
+        if len(embedding) == 1:
+            embeddings.append(embedding)
+            imgs.append(i)
+    pickle.dump(embeddings, open('face_dataset_embeddings_v2.p', 'wb') )
+    pickle.dump(imgs, open('img_names_v2.p', 'wb') )
+
+imgs = pickle.load( open ('img_names_v2.p', 'rb') )
+embeddings = pickle.load( open ('face_dataset_embeddings_v2.p', 'rb') )
+
+for i in range(len(imgs)):
+    index = imgs[i][0:5].find('_')
+    ID = imgs[i][0:index]
+    if ID not in IDs:
+        indexes.append(i)
+        IDs.append(ID)
+    buffer.append(ID)
+most_common = [item for item in Counter(buffer).most_common()]
+
+if GET_EMBEDDINGS:
+    dfs = []
+    for i in range(len(embeddings)):
+        d = pd.DataFrame(data=embeddings[i])
+        dfs.append(d)
+    df = pd.concat(dfs, ignore_index=True)
+    df.to_pickle("face_dataset_embeddings_df_v2.p")
+
+df = pd.read_pickle("face_dataset_embeddings_df_v2.p")
+
+print(most_common)
+print("Used images:",len(imgs))
+
+for i in range(len(IDs)):
+
+    #Create labels
+    labels = []
+    if i == len(IDs)-1:
+        for j in range(len(df.index.values)):
+            if j >= indexes[i]:
+                labels.append(1)
+            else:
+                labels.append(0)
+    else:
+        for j in range(len(df.index.values)):
+            if j >= indexes[i] and j < indexes[i+1]:
+                labels.append(1)
+            else:
+                labels.append(0)
+
+    #Train RandomForestClassifier
+    X_train, X_test, y_train, y_test = train_test_split(df, labels, test_size=0.3)
+    rf_classifier = RandomForestClassifier(n_estimators=100)
+    rf_classifier.fit(X_train,y_train)
+    y_pred = rf_classifier.predict(X_test)
+
+    print("Accuracy:", metrics.accuracy_score(y_test, y_pred))
+    print("Confusion matrix: \n", metrics.confusion_matrix(y_test, y_pred))
+    print("Recall:", metrics.recall_score(y_test, y_pred))
